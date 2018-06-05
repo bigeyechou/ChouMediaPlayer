e09f4c487c1fdbc1ee78db4ee4614b7c96062dfe
zhengguanzhou <bigeye_chou@163.ccom>
2018-06-05 19:11:26 +0800

    [guanZhou] 列表播放器样式

--- app/src/main/java/com/chou/android/choumediaplayer/VideoListActivity.java
+++ app/src/main/java/com/chou/android/choumediaplayer/VideoListActivity.java
@@ -1,7 +1,9 @@
 package com.chou.android.choumediaplayer;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * 列表模式的Video
  */
-public class VideoListActivity {
+public class VideoListActivity extends AppCompatActivity{
 }
