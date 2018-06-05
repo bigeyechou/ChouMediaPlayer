0481c1e93ad904de4aedf42daef50ba4a512e182
zhengguanzhou <bigeye_chou@163.com>
2018-06-05 19:15:00 +0800

    [guanZhou] 测试一下

--- app/src/main/java/com/chou/android/choumediaplayer/VideoListActivity.java
+++ app/src/main/java/com/chou/android/choumediaplayer/VideoListActivity.java
@@ -1,9 +1,29 @@
 package com.chou.android.choumediaplayer;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * 列表模式的Video
  */
 public class VideoListActivity extends AppCompatActivity{
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+    }
 }
