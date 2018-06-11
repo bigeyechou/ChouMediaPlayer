ff4f31b3e122962ede014979a3d43e595e1d1fef
zhengguanzhou <bigeye_chou@163.com>
2018-06-11 14:51:08 +0800

    【chou】 text

--- app/src/main/java/com/chou/android/choumediaplayer/VideoDetailActivity.java
+++ app/src/main/java/com/chou/android/choumediaplayer/VideoDetailActivity.java
@@ -1,9 +1,12 @@
 package com.chou.android.choumediaplayer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import com.chou.android.choumediaplayer.utils.NetUtils;
@@ -14,10 +17,13 @@ import com.danikula.videocache.HttpProxyCacheServer;
 
 import static com.chou.android.choumediaplayer.app.App.getProxy;
 
-public class VideoDetailActivity extends AppCompatActivity implements AtVideoPlayerController.OnVideoDetailListener{
+public class VideoDetailActivity extends AppCompatActivity
+    implements AtVideoPlayerController.OnVideoDetailListener {
 
     @Bind(R.id.video) ChouVideoPlayer video;
-    private String videoPath = "http://aliyunvideo.wujike.com.cn/3b4aa75e3c1b4df9bd268b67a50bfdf6/9ab7363c6422430f9d1f58e7849b281d-2f3d59ee927c4f91d67789ed134d127d-sd.mp4";
+    @Bind(R.id.btn_detail) Button btnDetail;
+    private String videoPath
+        = "http://aliyunvideo.wujike.com.cn/3b4aa75e3c1b4df9bd268b67a50bfdf6/9ab7363c6422430f9d1f58e7849b281d-2f3d59ee927c4f91d67789ed134d127d-sd.mp4";
     private String proxyPath;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,7 +31,12 @@ public class VideoDetailActivity extends AppCompatActivity implements AtVideoPla
         setContentView(R.layout.activity_video_detail_layout);
         ButterKnife.bind(this);
         initVideo();
-
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Intent intent = new Intent(VideoDetailActivity.this,VideoDetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -38,14 +49,14 @@ public class VideoDetailActivity extends AppCompatActivity implements AtVideoPla
         controller.setOnVideoDetailListener(this);
         video.setController(controller);
         NetUtils.setContext(this);
-        if (NetUtils.isNetworkConnected()){
-            if (NetUtils.isWiFiActive()){
+        if (NetUtils.isNetworkConnected()) {
+            if (NetUtils.isWiFiActive()) {
                 video.start();
-            }else {
+            } else {
                 //非WiFi
                 video.start();
             }
-        }else {
+        } else {
             //没有网络
             video.start();
         }
