8e5a6f613a6de8ad345e4b1e76c6ff1c6ed74d29
zhengguanzhou <bigeye_chou@163.com>
2018-06-14 15:58:18 +0800

    【chou】 取消详情页按钮

--- app/src/main/java/com/chou/android/choumediaplayer/VideoDetailActivity.java
+++ app/src/main/java/com/chou/android/choumediaplayer/VideoDetailActivity.java
@@ -21,7 +21,6 @@ public class VideoDetailActivity extends AppCompatActivity
     implements AtVideoPlayerController.OnVideoDetailListener {
 
     @Bind(R.id.video) ChouVideoPlayer video;
-    @Bind(R.id.btn_detail) Button btnDetail;
     private String videoPath
         = "http://aliyunvideo.wujike.com.cn/3b4aa75e3c1b4df9bd268b67a50bfdf6/9ab7363c6422430f9d1f58e7849b281d-2f3d59ee927c4f91d67789ed134d127d-sd.mp4";
     private String proxyPath;
@@ -31,12 +30,7 @@ public class VideoDetailActivity extends AppCompatActivity
         setContentView(R.layout.activity_video_detail_layout);
         ButterKnife.bind(this);
         initVideo();
-        btnDetail.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                Intent intent = new Intent(VideoDetailActivity.this,VideoDetailActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 
 
